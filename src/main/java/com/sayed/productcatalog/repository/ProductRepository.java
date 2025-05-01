6facc37c5ae7a48d55783833c0a32d675ee5ed6a
touatisayed <touati.sayed47@gmail.com>
2025-05-01 21:20:40 +0100

    Added @Repository annotation and import for ProductRepository
    - Included the @Repository annotation to mark ProductRepository as a Spring Data repository.
    - Imported org.springframework.stereotype.Repository to enable repository functionality.

--- src/main/java/com/sayed/productcatalog/repository/ProductRepository.java
+++ src/main/java/com/sayed/productcatalog/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.sayed.productcatalog.repository;
 
 import com.sayed.productcatalog.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // No need to write any methods — JpaRepository gives you CRUD by default
 }
